96c22712b587041fcff4cbba8edb71eec5a8aafc
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-09 15:32:03 +0600

    feat: update sidebar implementation and configuration
    - Changed the main entry point in package.json from extension.js to extension.cjs.
    - Updated build scripts in package.json to use esbuild for production and added type checking.
    - Added a new sidebar.html file for the sidebar UI with structured HTML and CSS.
    - Modified SidebarProvider to fetch sidebar content from a remote URL instead of embedding HTML directly.
    - Added sidebar URL to the configuration for dynamic loading of the sidebar content.

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,13 @@ const configDir = path.join(homeDir, '.create-it');
 const templatesFile = path.join(configDir, 'templates.json');
 const defaultTemplateUrl =
 	'https://github.com/Joint-Venture-AI/create-it/blob/main/resources/teamplates.json?raw=true';
+const sidebarUrl =
+	'https://raw.githubusercontent.com/Joint-Venture-AI/create-it/main/resources/sidebar.html';
 
 export const config = {
 	homeDir,
 	configDir,
 	templatesFile,
 	defaultTemplateUrl,
+	sidebarUrl,
 };
